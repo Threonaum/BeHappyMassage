8b9316038008d6abd044449f09b566c9d3656bdd
Threonaum <lepolak01@gmail.com>
2024-08-30 13:58:22 +0200

    update

---
+++ cgu.php
@@ -0,0 +1,150 @@
+<?php $page_id = 1; require "@tyrositeframework/start.php"; ?>
+
+
+
+
+<header> <?php $cp_navbarcgu(); ?> </header>
+
+<main>
+
+<div class="container cgu">
+        <h1>Politique de Confidentialité</h1>
+        <section>
+            <h2>1. Informations collectées</h2>
+            <p>Nous collectons les informations suivantes : Nom, prénom, adresse e-mail, numéro de téléphone, moyen de paiement. Ces informations sont fournies par vous via les formulaires que vous remplissez sur notre site.</p>
+        </section>
+        <section>
+            <h2>2. Utilisation des informations</h2>
+            <p>Les informations collectées sont utilisées pour fournir un service de bon cadeau et répondre à vos demandes.</p>
+        </section>
+        <section>
+            <h2>3. Partage des informations</h2>
+            <p>Les informations collectées ne sont pas partagées avec des tiers.</p>
+        </section>
+        <section>
+            <h2>4. Sécurité des informations</h2>
+            <p>Vos informations sont conservées dans un espace sécurisé auquel seul nous avons accès.</p>
+        </section>
+        <section>
+            <h2>5. Accès et rectification</h2>
+            <p>Vous pouvez accéder à vos informations et demander leur rectification en nous contactant directement via le formulaire de contact ou par téléphone.</p>
+        </section>
+        <section>
+            <h2>6. Utilisation des cookies</h2>
+            <p>Nous n'utilisons pas de cookies sur ce site.</p>
+        </section>
+        <section>
+            <h2>7. Contact</h2>
+            <p>Pour toute question concernant cette politique de confidentialité, vous pouvez nous contacter via notre formulaire de contact ou par téléphone.</p>
+        </section>
+        <section>
+            <h2>8. Modifications de la politique de confidentialité</h2>
+            <p>Nous nous réservons le droit de modifier cette politique de confidentialité. Toute modification sera publiée sur cette page.</p>
+        </section>
+    
+        <h1>Mentions Légales</h1>
+            
+            <section>
+                <h2>1. Éditeur du Site</h2>
+                <p><strong>Nom de l'Entreprise :</strong> Be Happy Massage</p>
+                <p><strong>Forme Juridique :</strong> [Indiquer la forme juridique, par exemple : SAS, SARL, etc.]</p>
+                <p><strong>Siège Social :</strong> 671 Rue Aimé Bonneville 01200 Valserhône</p>
+                <p><strong>Numéro SIRET :</strong> [Numéro SIRET]</p>
+                <p><strong>Numéro de TVA Intracommunautaire :</strong> [Numéro de TVA]</p>
+                <p><strong>Téléphone :</strong> 06 01 85 49 03</p>
+                <p><strong>Directeur de la Publication :</strong> DELANOUE Virginie</p>
+            </section>
+            
+            <section>
+                <h2>2. Hébergement</h2>
+                <p><strong>Nom de l'Hébergeur :</strong> [Nom de l'hébergeur]</p>
+                <p><strong>Adresse :</strong> [Adresse de l'hébergeur]</p>
+                <p><strong>Téléphone :</strong> [Numéro de téléphone de l'hébergeur]</p>
+                <p><strong>Adresse e-mail :</strong> [Adresse e-mail de l'hébergeur]</p>
+            </section>
+            
+            <section>
+                <h2>3. Propriété Intellectuelle</h2>
+                <p>Tous les éléments présents sur ce site, y compris les textes, images, graphiques, logos, vidéos, etc., sont la propriété exclusive de Be Happy Massage ou de ses partenaires ou libre de droits et sont protégés par les lois sur la propriété intellectuelle. Toute reproduction, distribution, modification ou utilisation non autorisée de ces éléments est interdite.</p>
+            </section>
+            
+            <section>
+                <h2>4. Responsabilité</h2>
+                <p>Be Happy Massage met tout en œuvre pour assurer l'exactitude des informations diffusées sur ce site. Cependant, nous ne pouvons garantir leur exhaustivité et leur actualité. En conséquence, nous déclinons toute responsabilité pour les erreurs, omissions ou imprécisions présentes sur le site, ainsi que pour les éventuels dommages directs ou indirects résultant de l'utilisation de ces informations.</p>
+            </section>
+            
+            <section>
+                <h2>5. Protection des Données Personnelles</h2>
+                <p>Les informations recueillies sur ce site sont destinées à Be Happy Massage dans le cadre de la gestion des demandes de contact et de la fourniture de services. Elles sont traitées conformément à notre Politique de Confidentialité.</p>
+            </section>
+            
+            <section>
+                <h2>6. Cookies</h2>
+                <p>Ce site n'utilise pas de cookies.</p>
+            </section>
+            
+            <section>
+                <h2>7. Contact</h2>
+                <p>Pour toute question relative au site, aux mentions légales ou à la Politique de Confidentialité, veuillez nous contacter :</p>
+                <p><strong>Via le formulaire de contact</strong></p>
+                <p><strong>Par téléphone :</strong>06 01 85 49 03</p>
+            </section>
+            
+            <section>
+                <h2>8. Modifications des Mentions Légales</h2>
+                <p>Be Happy Massage se réserve le droit de modifier les présentes mentions légales à tout moment. Les modifications seront publiées sur cette page et entreront en vigueur dès leur publication.</p>
+            </section>
+        
+        <h1>Conditions Générales d'Utilisation</h1>
+
+        <section>
+            <h2>1. Description des Services</h2>
+            <p>Ce site web est un site vitrine qui présente les prestations de massage offertes par Be Happy Massage. Il ne propose pas de réservation ou de paiement en ligne pour les séances de massage.</p>
+        </section>
+
+        <section>
+            <h2>2. Accès au Site</h2>
+            <p>L'accès au site est gratuit. Vous pouvez consulter les informations disponibles sans frais.</p>
+        </section>
+
+        <section>
+            <h2>3. Responsabilités et Limitations</h2>
+            <p>Be Happy Massage ne peut garantir l'exactitude et l'exhaustivité des informations fournies sur ce site. Nous ne sommes pas responsables des erreurs, omissions, ou dommages liés à l'utilisation du site. L'utilisation du site se fait à vos propres risques.</p>
+        </section>
+
+        <section>
+            <h2>4. Propriété Intellectuelle</h2>
+            <p>Les contenus du site, y compris les textes, images et logos, sont la propriété de Be Happy Massage ou sont utilisés avec l'autorisation des détenteurs des droits. Certains contenus peuvent être libres de droits.</p>
+        </section>
+
+        <section>
+            <h2>5. Utilisation du Site</h2>
+            <p>Vous vous engagez à utiliser le site de manière légale et à ne pas perturber son fonctionnement. L'utilisation abusive, le piratage ou la copie non autorisée des contenus sont interdits.</p>
+        </section>
+
+        <section>
+            <h2>6. Modifications des CGU</h2>
+            <p>Les Conditions Générales d'Utilisation peuvent être modifiées à tout moment. Les utilisateurs sont invités à consulter régulièrement cette page pour se tenir informés des modifications.</p>
+        </section>
+
+        <section>
+            <h2>7. Loi Applicable et Juridiction</h2>
+            <p>Les présentes CGU sont régies par la législation française. En cas de litige, les autorités compétentes seront les tribunaux français.</p>
+        </section>
+
+        <section>
+            <h2>8. Contact</h2>
+            <p>Pour toute question relative aux Conditions Générales d'Utilisation, veuillez nous contacter via le formulaire de contact sur le site ou par téléphone.</p>
+        </section>
+    
+</div>
+
+</main>
+
+
+
+
+
+
+
+<?php require "@tyrositeframework/end.php"; ?>
